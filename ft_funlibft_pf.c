a5ef247c8e18a11862f48b296b10b3dcb1c9c16e
Tomestre <gabrielveleci@gmail.com>
2025-02-16 15:45:25 +0000

    deleted comments

--- ft_funlibft_pf.c
+++ ft_funlibft_pf.c
@@ -6,7 +6,7 @@
 /*   By: gtome-ve <gtome-ve@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/09 20:29:30 by gtome-ve          #+#    #+#             */
-/*   Updated: 2025/02/14 22:31:09 by gtome-ve         ###   ########.fr       */
+/*   Updated: 2025/02/16 15:36:25 by gtome-ve         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,74 +14,73 @@
 
 size_t ft_strlen_pf(char *str)
 {
-    int i = -1;
-    while (str[++i])
-        ;
-    return (i);
+	int i = -1;
+	while (str[++i]);
+	return (i);
 }
 
 void ft_putchar_pf(char c, int *count)
 {
-    if (!count || *count == -1)
-        return;
-    if (write(1, &c, 1) == -1)
-        *count = -1;
-    else
-        (*count)++;
+	if (!count || *count == -1)
+		return;
+	if (write(1, &c, 1) == -1)
+		*count = -1;
+	else
+		(*count)++;
 }
 
 void ft_putdata_pf(long long nbr, char *base, int *count, int is_signed)
 {
-    size_t base_len;
+	size_t base_len;
 
-    if (is_signed && nbr < 0)
-    {
-        ft_putchar_pf('-', count);
-        nbr *= -1;
-    }
+	if (is_signed && nbr < 0)
+	{
+		ft_putchar_pf('-', count);
+		nbr *= -1;
+	}
 
-    base_len = ft_strlen_pf(base);
+	base_len = ft_strlen_pf(base);
 
-    if ((unsigned long long)nbr < base_len)
-    {
-        ft_putchar_pf(base[(unsigned long long)nbr], count); 
-    }
-    else
-    {
-        ft_putdata_pf(nbr / base_len, base, count, is_signed);
-        ft_putdata_pf(nbr % base_len, base, count, is_signed);
-    }
+	if ((unsigned long long)nbr < base_len)
+	{
+		ft_putchar_pf(base[(unsigned long long)nbr], count);
+	}
+	else
+	{
+		ft_putdata_pf(nbr / base_len, base, count, is_signed);
+		ft_putdata_pf(nbr % base_len, base, count, is_signed);
+	}
 }
 
 
 void ft_putstr_or_ptr_pf(char *str, int *count, int is_ptr)
 {
-    if (!count)
-        return;
-    if (!str)
-    {
-        if (is_ptr)
-            str = "(nil)";
-        else
-            str = "(null)";
-    }
-    else if (is_ptr) // Para ponteiros, adiciona o prefixo "0x"
-    {
-        ft_putstr_pf("0x", count);
-        ft_putdata_pf((unsigned long long)(uintptr_t)str, HEX_LOW_BASE, count, 0); // Trata como unsigned long long
-        return;
-    }
+	if (!count)
+		return;
+	if (!str)
+	{
+		if (is_ptr)
+			str = "(nil)";
+		else
+			str = "(null)";
+	}
+	else if (is_ptr)
+	{
+		ft_putstr_pf("0x", count);
+		ft_putdata_pf((unsigned long long)str, HEX_LOW_BASE, count, 0);
+		return;
+	}
 
-    // Imprime cada caractere da string
-    while (*str)
-    {
-        ft_putchar_pf(*str, count);
-        str++;
-    }
+
+	while (*str)
+	{
+		ft_putchar_pf(*str, count);
+		str++;
+	}
 }
 
-// Função auxiliar para imprimir strings (reutilizada em ft_putstr_or_ptr_pf)
+
 void ft_putstr_pf(char *str, int *count)
 {
-    ft_putstr_or_ptr_pf(str, count, 0); // Chama ft_putstr_or_ptr_pf com is_ptr = 0
+	ft_putstr_or_ptr_pf(str, count, 0);
 }
--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: gtome-ve <gtome-ve@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/09 20:31:11 by gtome-ve          #+#    #+#             */
-/*   Updated: 2025/02/14 22:34:42 by gtome-ve         ###   ########.fr       */
+/*   Updated: 2025/02/16 15:39:13 by gtome-ve         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,71 +14,71 @@
 
 int ft_fillformat_pf(va_list lst, char c, int *count)
 {
-    if (!count)
-        return (-1);
+	if (!count)
+		return (-1);
 
-    if (c == 'c') // Caractere
-    {
-        ft_putchar_pf((char)va_arg(lst, int), count);
-    }
-    else if (c == 's') // String
-    {
-        ft_putstr_or_ptr_pf(va_arg(lst, char *), count, 0); // Usa is_ptr = 0 para strings
-    }
-    else if (c == 'p') // Ponteiro
-    {
-        ft_putstr_or_ptr_pf(va_arg(lst, void *), count, 1); // Usa is_ptr = 1 para ponteiros
-    }
-    else if (c == 'd' || c == 'i') // Inteiro com sinal
-    {
-        ft_putdata_pf((long long)va_arg(lst, int), INT_BASE, count, 1);
-    }
-    else if (c == 'u') // Inteiro sem sinal
-    {
-        ft_putdata_pf((unsigned long long)va_arg(lst, unsigned int), INT_BASE, count, 0);
-    }
-    else if (c == 'x') // Hexadecimal minúsculo
-    {
-        ft_putdata_pf((unsigned long long)va_arg(lst, unsigned int), HEX_LOW_BASE, count, 0);
-    }
-    else if (c == 'X') // Hexadecimal maiúsculo
-    {
-        ft_putdata_pf((unsigned long long)va_arg(lst, unsigned int), HEX_UPP_BASE, count, 0);
-    }
-    else if (c == '%') // Porcentagem
-    {
-        ft_putchar_pf('%', count);
-    }
+	if (c == 'c')
+	{
+		ft_putchar_pf((char)va_arg(lst, int), count);
+	}
+	else if (c == 's')
+	{
+		ft_putstr_or_ptr_pf(va_arg(lst, char *), count, 0);
+	}
+	else if (c == 'p')
+	{
+		ft_putstr_or_ptr_pf(va_arg(lst, void *), count, 1);
+	}
+	else if (c == 'd' || c == 'i')
+	{
+		ft_putdata_pf((long long)va_arg(lst, int), INT_BASE, count, 1);
+	}
+	else if (c == 'u')
+	{
+		ft_putdata_pf((unsigned long long)va_arg(lst, unsigned int), INT_BASE, count, 0);
+	}
+	else if (c == 'x')
+	{
+		ft_putdata_pf((unsigned long long)va_arg(lst, unsigned int), HEX_LOW_BASE, count, 0);
+	}
+	else if (c == 'X')
+	{
+		ft_putdata_pf((unsigned long long)va_arg(lst, unsigned int), HEX_UPP_BASE, count, 0);
+	}
+	else if (c == '%')
+	{
+		ft_putchar_pf('%', count);
+	}
 
-    return (0);
+	return (0);
 }
 
 int ft_printf(char const *str, ...)
 {
-    va_list lst;
-    int     count;
-    int     i;
+	va_list lst;
+	int     count;
+	int     i;
 
-    count = 0;
-    i = 0;
-    if (!str)
-        return (-1);
+	count = 0;
+	i = 0;
+	if (!str)
+		return (-1);
 
-    va_start(lst, str);
-    while (str[i] && count != -1)
-    {
-        if (str[i] != '%')
-        {
-            ft_putchar_pf(str[i], &count);
-        }
-        else
-        {
-            i++;
-            ft_fillformat_pf(lst, str[i], &count);
-        }
-        i++;
-    }
-    va_end(lst);
+	va_start(lst, str);
+	while (str[i] && count != -1)
+	{
+		if (str[i] != '%')
+		{
+			ft_putchar_pf(str[i], &count);
+		}
+		else
+		{
+			i++;
+			ft_fillformat_pf(lst, str[i], &count);
+		}
+		i++;
+	}
+	va_end(lst);
 
-    return (count);
+	return (count);
 }
